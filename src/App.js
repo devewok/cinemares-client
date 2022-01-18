d14466afd703c1e24f57bcc487319e7502e06a08
Brandon Jaramillo <bsjaramillo1995@gmail.com>
2022-01-18 15:02:04 -0500

    Change color animation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import {useRef, useState} from 'react';
 import './App.css';
 import MovieScreen from './components/screens/MovieScreen';
 import LoginScreen from './components/screens/LoginScreen';
-import {CSSTransition} from 'react-transition-group';
 import {SwitchTransition, Transition} from 'react-transition-group';
 import styled from "styled-components";
 
